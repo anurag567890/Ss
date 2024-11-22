11cf9ca96dd223359efed14231f643a1d47382e8
anurag567890 <mh15nashikb.@gmail.com>
2024-11-22 22:55:46 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY SHARP
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7646468262:AAEheqVqBHFxGX20wwNmOQ_uRK8F5bDVowc")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@ITS_KAAL_01")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7825026453")
+
+
+
+
+
+
+
+  
